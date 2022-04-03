7875b0a3fc5b0c61efbb153594dcf98c47e3571d
banna12345bob <banna12345bob@outlook.com>
2022-04-03 14:37:54 +1000

    Fixed premake
    Premake wasn't generating the files correctly so I fixed that

--- premake5.lua
+++ premake5.lua
@@ -1,105 +1,105 @@
 workspace "Solus"
-    architecture "x64"
+	architecture "x64"
 
-    configurations
-    {
-        "Debug",
-        "Release",
-        "Dist"
-    }
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 project "Solus"
-    location "Solus"
-    kind "SharedLib"
-    language "C++"
+	location "Solus"
+	kind "SharedLib"
+	language "C++"
 
-    targetdir ("compile/bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("compile/bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("compile/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("compile/bin-int/" .. outputdir .. "/%{prj.name}")
 
-    files
-    {
-        "%{prj.name}/src/**.h",
-        "%{prj.name}/src/**.cpp",
-    }
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
 
-    includedirs
-    {
-        "%{prj.name}/vendor/spdlog/include"
-    }
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
 
-    filter "system:windows"
-        cppdialect "C++17"
-        staticruntime "On"
-        systemversion "latest"
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
 
-        defines
-        {
-            "SU_PLATFORM_WINDOWS",
-            "SU_BUILD_DLL"
-        }
+		defines
+		{
+			"SU_PLATFORM_WINDOWS",
+			"SU_BUILD_DLL"
+		}
 
-        postbuildcommands
-        {
-            ("{COPY} %{cfg.buildtarget.relpath} ../compile/bin/" .. outputdir .. "/Sandbox")
-        }
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../compile/bin/" .. outputdir .. "/Sandbox")
+		}
 
-    filter "configurations:Debug"
-        defines "HZ_DEBUG"
-        symbols "On"
+	filter "configurations:Debug"
+		defines "SU_DEBUG"
+		symbols "On"
 
-    filter "configurations:Release"
-        defines "HZ_RELEASE"
-        optimize "On"
+	filter "configurations:Release"
+		defines "SU_RELEASE"
+		optimize "On"
 
-    filter "configurations:Dist"
-        defines "HZ_DIST"
-        optimize "On"
+	filter "configurations:Dist"
+		defines "SU_DIST"
+		optimize "On"
 
 project "Sandbox"
-        location "Sandbox"
-        kind "ConsoleApp"
-        language "C++"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
 
-    targetdir ("compile/bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("compile/bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("compile/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("compile/bin-int/" .. outputdir .. "/%{prj.name}")
 
-    files
-    {
-        "%{prj.name}/src/**.h",
-        "%{prj.name}/src/**.cpp",
-    }
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
 
-    includedirs
-    {
-        "Solus/vendor/spdlog/include",
-        "Solus/src"
-    }
+	includedirs
+	{
+		"Solus/vendor/spdlog/include",
+		"Solus/src"
+	}
 
-    links
-    {
-        "Solus"
-    }
+	links
+	{
+		"Solus"
+	}
 
-    filter "system:windows"
-        cppdialect "C++17"
-        staticruntime "On"
-        systemversion "latest"
+filter "system:windows"
+	cppdialect "C++17"
+	staticruntime "On"
+	systemversion "latest"
 
-        defines
-        {
-            "SU_PLATFORM_WINDOWS",
-        }
+	defines
+	{
+		"SU_PLATFORM_WINDOWS"
+	}
 
-    filter "configurations:Debug"
-        defines "HZ_DEBUG"
-        symbols "On"
+	filter "configurations:Debug"
+		defines "SU_DEBUG"
+		symbols "On"
 
-    filter "configurations:Release"
-        defines "HZ_RELEASE"
-        optimize "On"
+	filter "configurations:Release"
+		defines "SU_RELEASE"
+		optimize "On"
 
-    filter "configurations:Dist"
-        defines "HZ_DIST"
-        optimize "On"
+	filter "configurations:Dist"
+		defines "SU_DIST"
+		optimize "On"
