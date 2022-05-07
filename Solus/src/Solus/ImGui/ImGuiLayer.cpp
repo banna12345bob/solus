14ea3c4efd7add38a5f5171b63fbc759778e7988
banna12345bob <banna12345bob@outlook.com>
2022-05-08 09:00:21 +1000

    Release and Dist now build without console

--- Solus/src/Solus/ImGui/ImGuiLayer.cpp
+++ Solus/src/Solus/ImGui/ImGuiLayer.cpp
@@ -87,8 +87,8 @@ namespace Solus {
 
 	void ImGuiLayer::OnImGuiRender()
 	{
-		/*static bool show = true;
-		ImGui::ShowDemoWindow(&show);*/
+		static bool show = true;
+		ImGui::ShowDemoWindow(&show);
 	}
 
 }
