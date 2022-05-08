d4aaec61db4c146fbda1b88c7e3f60f72a4e47c7
banna12345bob <banna12345bob@outlook.com>
2022-05-08 13:06:27 +1000

    Logger disabled in release build

--- Solus/src/Solus/Core/Log.h
+++ Solus/src/Solus/Core/Log.h
@@ -22,6 +22,21 @@ namespace Solus {
 
 }
 
+#ifdef SU_RELASE
+/* ------------------------- Core log macros ------------------------- */
+#define SU_CORE_TRACE(...)
+#define SU_CORE_INFO(...)
+#define SU_CORE_WARN(...)
+#define SU_CORE_ERROR(...)
+#define SU_CORE_FATAL(...)
+
+/* ------------------------ Client log macros ------------------------ */
+#define SU_TRACE(...)
+#define SU_INFO(...)
+#define SU_WARN(...)
+#define SU_ERROR(...)
+#define SU_FATAL(...)
+#else
 /* ------------------------- Core log macros ------------------------- */
 #define SU_CORE_TRACE(...)   ::Solus::Log::GetCoreLogger()->trace(__VA_ARGS__)
 #define SU_CORE_INFO(...)    ::Solus::Log::GetCoreLogger()->info(__VA_ARGS__)
@@ -35,3 +50,4 @@ namespace Solus {
 #define SU_WARN(...)         ::Solus::Log::GetClientLogger()->warn(__VA_ARGS__)
 #define SU_ERROR(...)        ::Solus::Log::GetClientLogger()->error(__VA_ARGS__)
 #define SU_FATAL(...)        ::Solus::Log::GetClientLogger()->critical(__VA_ARGS__)
+#endif
