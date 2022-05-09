085d3451bdf9d2095c43d5f814a9ba93d7357000
banna12345bob <banna12345bob@outlook.com>
2022-05-09 15:59:02 +1000

    Started creating shaders
    Up to about 19 minutes on the shader video

---
+++ Solus/src/Solus/Renderer/Shader.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "supch.h"
+
+namespace Solus {
+
+	class Shader
+	{
+	public:
+		Shader(const std::string& vertexSrc, const std::string& fragmentSrc);
+		~Shader();
+
+		void Bind() const;
+		void Unbind() const;
+	private:
+		uint32_t m_RendererID;
+	};
+
+}
